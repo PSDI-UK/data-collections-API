7b94260a0ca2e95e8c090233eb34058cd4f8585b
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2025-04-29 15:23:36 +0100

    Add metadata validation

--- data_collections_api/invenio.py
+++ data_collections_api/invenio.py
@@ -838,7 +838,7 @@ class _Licenses(_SubCommandHandler):
 class InvenioRepository:
     """Handler for Invenio-like repositories.
 
-    Handles pushing info to e.g. Zenodo
+    Handles pushing info to e.g. Zenodo.
 
     Parameters
     ----------

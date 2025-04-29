fbc9bde3fb88aa337743fc17f868f3c067e80059
ElliottKasoar <45317199+ElliottKasoar@users.noreply.github.com>
2025-04-29 10:55:10 +0100

    Apply pre-commmit

--- data_collections/__init__.py
+++ data_collections/__init__.py
@@ -1,4 +1,6 @@
 """Data API for uploading and downloading from PSDI depositories."""
 
+from __future__ import annotations
+
 __version__ = "0.1.0"
 __author__ = "Jacob Wilkins, Elliot Kasoar, Jas Kalaya, Alin Elena"

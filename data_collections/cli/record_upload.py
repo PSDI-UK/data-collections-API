7bf098d54b7ab99ea6c4062f99e18e0d9633469d
Jas Kalayan <jkalayan@gmail.com>
2025-09-16 15:19:23 +0100

    remove trys in main and better type hints

--- data_collections/cli/record_upload.py
+++ data_collections/cli/record_upload.py
@@ -5,16 +5,14 @@ from __future__ import annotations
 
 import argparse
 import glob
-import logging
 from pathlib import Path
-import sys
 
 import yaml
 
 from data_collections.invenio import InvenioRepository
 
 
-def create_files_dict(all_files: list):
+def create_files_dict(all_files: list[Path | str]):
     """
     Save file paths into a dictionary to a format e.g.
 
@@ -32,7 +30,7 @@ def create_files_dict(all_files: list):
     files_dict = {}
     for file_str in all_files:
         # expand file_str if using wildcards
-        files = glob.glob(file_str)
+        files = glob.glob(file_str)  # noqa: PTH207
         for file in files:
             file_path = Path(file)
             files_dict[file_path.name] = file_path
@@ -78,53 +76,46 @@ def run_record_upload(
 
 def main():
     """Upload records to Invenio repository."""
-    try:
-        usage = "upload_record [-h]"
-        parser = argparse.ArgumentParser(
-            description="Upload records to Invenio repository",
-            usage=usage,
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-        )
-        parser.add_argument_group("Options")
-        parser.add_argument(
-            "--api_url",
-            metavar="str",
-            default=None,
-            required=True,
-            help="url for the API associated with the Invenio repository",
-        )
-        parser.add_argument(
-            "--api_key",
-            metavar="str",
-            default=None,
-            required=True,
-            help="key for accessing the Invenio repository",
-        )
-        parser.add_argument(
-            "--metadata_path",
-            metavar="file",
-            default=None,
-            required=True,
-            help="file path for Invenio metadata yaml file",
-        )
-        parser.add_argument(
-            "--files",
-            nargs="+",
-            help="list of file paths associated with the record to be uploaded",
-        )
-        parser.add_argument(
-            "--community",
-            metavar="str",
-            default=None,
-            help="name of community to upload record to, e.g. biosimdb",
-        )
-        op = parser.parse_args()
-    except argparse.ArgumentError:
-        logging.error(
-            "Command line arguments are ill-defined, please check the arguments.",
-        )
-        raise
-        sys.exit(1)
+    usage = "upload_record [-h]"
+    parser = argparse.ArgumentParser(
+        description="Upload records to Invenio repository",
+        usage=usage,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument_group("Options")
+    parser.add_argument(
+        "--api_url",
+        metavar="str",
+        default=None,
+        required=True,
+        help="url for the API associated with the Invenio repository",
+    )
+    parser.add_argument(
+        "--api_key",
+        metavar="str",
+        default=None,
+        required=True,
+        help="key for accessing the Invenio repository",
+    )
+    parser.add_argument(
+        "--metadata_path",
+        metavar="file",
+        default=None,
+        required=True,
+        help="file path for Invenio metadata yaml file",
+    )
+    parser.add_argument(
+        "--files",
+        nargs="+",
+        help="list of file paths associated with the record to be uploaded",
+    )
+    parser.add_argument(
+        "--community",
+        metavar="str",
+        default=None,
+        help="name of community to upload record to, e.g. biosimdb",
+    )
+    op = parser.parse_args()
 
     run_record_upload(
         api_url=op.api_url,
